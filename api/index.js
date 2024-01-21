fa1faa11b15801995b90400083e0d00d6a1dca50
Polen <147795380+codingb21@users.noreply.github.com>
2024-01-21 22:08:15 +0700

    1

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import allDataRoute from "./routes/all_data.js";
 import mysql from "mysql2";
 import path from "path";
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "mydb",
