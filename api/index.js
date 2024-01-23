4a6f20aa4f38690c27d64aa009493db4d6804f6f
Polen <147795380+codingb21@users.noreply.github.com>
2024-01-24 00:07:23 +0700

    update

--- api/index.js
+++ api/index.js
@@ -3,23 +3,28 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import allDataRoute from "./routes/all_data.js";
 import path from "path";
+
 const app = express();
+const __dirname = path.resolve();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const __dirname = path.resolve();
-//middleware
-app.use("/all_data", allDataRoute);
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log("server is running on port " + PORT);
-});
-// Assuming your frontend is built in the "dist" directory
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
+// Serve static files from the "client/dist" directory
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+// Route for "/all_data"
+app.use("/all_data", allDataRoute);
+
+// Catch-all route, send "index.html" for any other routes
 app.get("*", (req, res) => {
-  // Update the path to match your built frontend
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
