5003273cc8e7f6a6c346fe6aa26da78484bfdc03
Polen <147795380+codingb21@users.noreply.github.com>
2024-01-23 23:13:35 +0700

    final

--- api/routes/all_data.js
+++ api/routes/all_data.js
@@ -1,16 +1,14 @@
 import express from "express";
 const router = express.Router();
-import mysql from "mysql2";
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: "",
   database: "mydb",
-  connectionLimit: 10, // Adjust as needed
 });
 
-db.createPool((err) => {
+db.connect((err) => {
   if (err) {
     console.error("MySQL connection error:", err);
   } else {
